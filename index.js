04b96dfe9e6c1097248e2ae0ca02e240cbe46ddf
Halima Ibrahim <sadeeyahibraheem@gmail.com>
2023-02-14 17:23:28 +0100

    update

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function devide(num1,  num2 )
 {
     return num1 / num2;
 }
+
+
+let  firstName = "Halima";
+let  lastName = "Ibrahim";
+let  stuternTrack = "Frontend Engineer";
+let  skill = "HTML, CSS, JavaScript, Figma, Photoshop and AdobeXD";
+let  color = "Blue";
+let  countryindependence = "1st October 1960";
+
+console.log('I am ' + firstName + " "  +  lastName + ' I am a ' + stuternTrack + ' my skills are ' + skill + ' my favorite color is ' +  color + ' and ' + ' my country independence date is ' + countryindependence);
